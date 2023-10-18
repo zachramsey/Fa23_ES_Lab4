c019fcfe4f0c1883498a07bebaefaf68b189baab
Zach Ramsey <zramsey11@gmail.com>
2023-10-18 12:25:47 -0500

    PWM initialization added

--- Fa23_ES_Lab4.asm
+++ Fa23_ES_Lab4.asm
@@ -19,38 +19,30 @@ cbi DDRD,6					; Board Pin 6 RPG A -> Board I/P: PD6
 cbi DDRD,5  				; Board Pin 5 RPG B -> Board I/P: PD5
 
 ;==============| Configure Registers |===============
-.def Disp_Queue = R16		; Data queue for next digit to be displayed
-.def Disp_Decr = R17		; Count of remaining bits to be pushed from Disp_Queue; decrements from 8
+.def Tmp_Reg = R16			; Temporary register
+.def Tmr_Cnt = R17			; Timer counter
 .def RPG_Curr = R18			; Current RPG input state
 .def RPG_Prev = R19			; previous RPG input state
-.def Ptrn_Cnt = R20			; Pattern counter
-.def Tmp_Reg = R21			; Temporary register
-.def Tmr_Cnt = R22			; Timer counter
-.def Btn_Cnt = R23			; Button timer counter
-
-;=========| Load Digit Patterns |==========
-rjmp Init					; don't execute data!
-Ptrns:
 
 ;===================| Main Loop |====================
 Init:
-	; init timer0 for PWM
-	ldi Tmp_Reg, 0x05		; configure prescaler to 1024
-	out TCCR0B, Tmp_Reg		; output configuration to TCCR0B
-	
-	;init timer 1 for Genereral use
-	ldi Tmp_Reg, 0x05
-	out TCCR2B, Tmp_Reg
+	rcall Init_Timer0		; init timer0 for PWM
+	rcall Init_Timer2		; init timer 2 for General use
+	rcall Init_LCD			; initialize LCD
 
-	;init LCD
-	;wait to power up 100ms
+Main:
+	rjmp Main				; loop Main
+
+Init_LCD:
+	; is there anything missing?
+	rcall delay_100ms		;wait 100ms
 	ldi Tmp_Reg,0x03		;Load hex 3 into register
 	out PORTC,Tmp_Reg		;Send command to lcd display
-	;wait 5ms
+	rcall delay_5ms			;wait 5ms
 	out PORTC,Tmp_Reg		;Send command to lcd display
-	;wait 200us
+	rcall delay_208us
 	out PORTC,Tmp_Reg		;Send command to lcd display
-	;wait 200us
+	rcall delay_208us
 	ldi Tmp_Reg,0x02		;Enable 4-bit mode
 	out PORTC,Tmp_Reg
 
@@ -66,82 +58,84 @@ Init:
 	out PORTC,Tmp_Reg
 	ldi Tmp_Reg,0x0C		;turn on display
 	out PORTC,Tmp_Reg
-Main:
+	ret
 
-	; loop Main
-	rjmp Main
+Init_Timer0:
+	ldi Tmp_Reg, 0x03		; configure timer0 to Fast PWM (mode 7)
+	out TCCR0A, Tmp_Reg
+	ldi Tmp_Reg, 0x08
+	out TCCR0B, Tmp_Reg
+	ldi Tmp_Reg, 200		; set timer0 TOP val to 200
+	out OCR0A, Tmp_Reg
+	ldi Tmp_Reg, 0			; set timer0 duty cycle to 0 (OCR0B = 200 * DC)
+	out OCR0B, Tmp_Reg
+	ret
+
+Init_Timer2:
+	ldi Tmp_Reg, 0x05
+	out TCCR2B, Tmp_Reg
+	ret
 
 ;delay_112us
 delay_112us:
-;init timer config
-	ldi Tmr_Cnt,7
+	ldi Tmr_Cnt,7			;init timer config
 	ldi Tmp_Reg, 0x01
 	out TCCR2B, Tmp_Reg
 
-	L1A:
-		L2A:
-			in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
-			sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
-			rjmp L2A
+	loop_100u:
+	in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
+	sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
+	rjmp loop_100u
 
-		dec Tmr_Cnt					; Decrement Timer counter
-		tst Tmr_Cnt					; Is Timer counter zero?
-		brne L1A					;No, jump to L1, Yes return
+	dec Tmr_Cnt				; Decrement Timer counter
+	tst Tmr_Cnt				; Is Timer counter zero?
+	brne loop_100u			; No, jump to L1, Yes return
 	ret
 
 ;delay_208us
 delay_208us:
-;init timer config
-	ldi Tmr_Cnt,13
+	ldi Tmr_Cnt,13			;init timer config
 	ldi Tmp_Reg, 0x01
 	out TCCR2B, Tmp_Reg
 
-	L1B:
-		L2B:
-			in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
-			sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
-			rjmp L2B
+	loop_200u:
+	in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
+	sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
+	rjmp loop_200u
 
-		dec Tmr_Cnt					; Decrement Timer counter
-		tst Tmr_Cnt					; Is Timer counter zero?
-		brne L1B					;No, jump to L1B, Yes return
-
+	dec Tmr_Cnt				; Decrement Timer counter
+	tst Tmr_Cnt				; Is Timer counter zero?
+	brne loop_200u			; No, jump to L1B, Yes return
 	ret
 
 ;delay_5.12ms
 delay_5ms:
-;init timer config
-	ldi Tmr_Cnt,40
+	ldi Tmr_Cnt,40			;init timer config
 	ldi Tmp_Reg, 0x02
 	out TCCR2B, Tmp_Reg
 
-	L1C:
-		L2C:
-			in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
-			sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
-			rjmp L2C
+	loop_5m:
+	in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
+	sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
+	rjmp loop_5m
 
-		dec Tmr_Cnt					; Decrement Timer counter
-		tst Tmr_Cnt					; Is Timer counter zero?
-		brne L1C					;No, jump to L1C, Yes return
-
+	dec Tmr_Cnt				; Decrement Timer counter
+	tst Tmr_Cnt				; Is Timer counter zero?
+	brne loop_5m			; No, jump to L1C, Yes return
 	ret
 
 ;delay_100.35ms
 delay_100ms:
-;init timer config
-	ldi Tmr_Cnt,98
+	ldi Tmr_Cnt,98			;init timer config
 	ldi Tmp_Reg, 0x03
 	out TCCR2B, Tmp_Reg
 
-	L1D:
-		L2D:
-			in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
-			sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
-			rjmp L2D
+	loop_100m:
+	in Tmp_Reg, TIFR2		; input timer2 interrupt flag register
+	sbrs Tmp_Reg, 0			; if overflow flag is not set, loop Running
+	rjmp loop_100m
 
-		dec Tmr_Cnt					; Decrement Timer counter
-		tst Tmr_Cnt					; Is Timer counter zero?
-		brne L1D					;No, jump to L1D, Yes return
-
+	dec Tmr_Cnt				; Decrement Timer counter
+	tst Tmr_Cnt				; Is Timer counter zero?
+	brne loop_100m			; No, jump to L1D, Yes return
 	ret
